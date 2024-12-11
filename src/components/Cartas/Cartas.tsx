10fa5cdcf0c40a9b18c4f6bafc075023abb81e9d
bongmin25 <tom.dantas25@gmail.com>
2024-12-11 15:28:37 -0300

    Styling Cards

--- src/components/Cartas/Cartas.tsx
+++ src/components/Cartas/Cartas.tsx
@@ -7,41 +7,49 @@ import AddToCartButton from "../AddToCartButton/AddToCartButton";
 const Cartas = () => {
   return (
     <div className="max-w-screen-xl mx-auto py-8 px-4">
-      <h1 className="text-3xl font-bold text-gray-800 ">Remeras Oversize</h1>
-      <p className=" text-gray-600 mt-4">
+      <h1 className="text-3xl font-bold text-gray-800">Remeras Oversize</h1>
+      <p className="text-gray-600 mt-4">
         Elegí tu remera JESCA, estilo sin esfuerzo, comodidad total
       </p>
-      <div className="grid gap-6 mt-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <div className="grid gap-2 mt-10 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
         {cards.map((card, i) => (
           <div
             key={i}
-            className="flex flex-col items-center shadow-lg rounded-lg p-4 border hover:shadow-xl transition-shadow duration-300"
+            className="group flex flex-col bg-white shadow-lg rounded-lg overflow-hidden border hover:shadow-xl transition-shadow duration-300"
           >
-            <Link href={`/remeras/${card.id}`}>
-              <div className="relative lg:h-60">
-                <Image
-                  src={card.image}
-                  alt={card.title}
-                  width={400}
-                  height={400}
-                  className="object-cover rounded transition-transform transform hover:scale-105 duration-300"
+            <Link href={`/remeras/${card.id}`} className="relative block">
+              {/* Imagen principal */}
+              <Image
+                src={card.image}
+                alt={card.title}
+                width={500}
+                height={500}
+                className="object-cover w-full h-72 lg:h-96 transition-opacity duration-300 group-hover:opacity-0"
+              />
+              {/* Imagen de hover */}
+              <Image
+                src="/portada1.jpeg" // Nueva propiedad en tus datos
+                alt={card.title}
+                width={500}
+                height={500}
+                className="absolute top-0 left-0 object-cover w-full h-72 lg:h-96 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+              />
+            </Link>
+
+            <div className="p-4">
+              <p className="text-lg font-medium text-gray-800">{card.title}</p>
+              <p className="text-lg font-bold text-gray-900 mt-2">$10,499</p>
+              <p className="text-sm text-gray-500 mt-1">
+                3 cuotas <strong>sin interés</strong> $3,500
+              </p>
+              <div className="mt-4 text-center">
+                <AddToCartButton
+                  id={card.id}
+                  title={card.title}
+                  image={card.image}
+                  price={10499}
                 />
               </div>
-            </Link>
-            <p className="text-gray-700 text-center lg:mt-60">{card.title}</p>
-            <p className="text-gray-800 text-center mt-2 font-bold text-lg">
-              $10,499
-            </p>
-            <p className="text-gray-500 text-center mt-1">
-              3 cuotas <strong>sin interés</strong> $3,500
-            </p>
-            <div className="mt-4">
-              <AddToCartButton
-                id={card.id}
-                title={card.title}
-                image={card.image}
-                price={10499}
-              />
             </div>
           </div>
         ))}
--- src/components/Inicio/Inicio.tsx
+++ src/components/Inicio/Inicio.tsx
@@ -4,13 +4,28 @@ export default function Inicio() {
   return (
     <div className="grid h-screen grid-rows-3 sm:grid-rows-1 sm:grid-cols-3">
       <div className="relative">
-        <Image src="/portada1.jpeg" alt="Imagen 1" fill objectFit="cover" />
+        <Image
+          src="/portada1.jpeg"
+          alt="Imagen 1"
+          fill
+          className="object-cover"
+        />
       </div>
       <div className="relative">
-        <Image src="/portada2.jpeg" alt="Imagen 2" fill objectFit="cover" />
+        <Image
+          src="/portada2.jpeg"
+          alt="Imagen 2"
+          fill
+          className="object-cover"
+        />
       </div>
       <div className="relative">
-        <Image src="/portada3.jpeg" alt="Imagen 3" fill objectFit="cover" />
+        <Image
+          src="/portada3.jpeg"
+          alt="Imagen 3"
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
